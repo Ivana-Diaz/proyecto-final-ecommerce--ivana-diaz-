c3e8290b4910ee50cd025eec732a48fa22006007
Ivana Diaz <ivanadiaz.v@gmail.com>
2025-11-02 14:58:18 -0300

    Creacion array de productos

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const productos = [
+    { imagen: "./img/pulover-gris.jpg", nombre: "Pulóver gris", precio: 1400 },
+    { imagen: "./img/campera-rosa.jpg", nombre: "Campera rosa", precio: 800 },
+    { imagen: "./img/pulover-blanco.jpg", nombre: "Pulóver blanco", precio: 2200 },
+    { imagen: "./img/cardigan-naranja.jpg", nombre: "Cárdigan naranja", precio: 790 },
+    { imagen: "./img/sueter-azul.jpg", nombre: "Suéter azul", precio: 1850 },
+    { imagen: "./img/sueter-verde.jpg", nombre: "Suéter verde", precio: 2000 },
+];
+
